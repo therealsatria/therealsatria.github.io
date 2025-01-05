c0482de41fa106f967d81d85626d7b940a29e579
therealsatria <36116945+therealsatria@users.noreply.github.com>
2025-01-05 08:59:05 +0700

    update 2025

---
+++ script.js
@@ -0,0 +1,6 @@
+let namaDepan = "Satria";
+let namaBelakang = "Nugraha";
+let namaLengkap = `${namaDepan} ${namaBelakang}`;
+
+let namaContainer = document.getElementById("name");
+namaContainer.textContent = namaLengkap;
