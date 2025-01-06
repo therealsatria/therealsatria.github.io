22efc2c00d40b530f861604d0fd77e2d9831030b
satria n <therealsatria@gmail.com>
2025-01-06 14:59:39 +0700

    update deskripsi

--- assets/js/satria.js
+++ assets/js/satria.js
@@ -2,7 +2,12 @@ const namaDepan = "Satria";
 const namaBelakang = "Nugraha";
 let namaLengkap = `${namaDepan} ${namaBelakang}`;
 let footerText = `© ${new Date().getFullYear()} ${namaLengkap}`;
-const deskripsi = "An experienced IT professional with 8+ years of dual expertise as a .NET Back End Developer and Microsoft Infrastructure Technical Consultant, specializing in the financial industry. Possesses a solid track record of developing reliable and scalable .NET Core and ASP.NET-based applications, as well as designing, implementing, and maintaining Microsoft infrastructure solutions, including Active Directory for centralized identity and access management, Azure for cloud migration and serverless services, and Microsoft 365 for enhanced collaboration and productivity. Successfully reduced application downtime by 30% through the implementation of high availability solutions in Azure. Dedicated to delivering innovative technology solutions that drive efficiency, security, and business growth. Seeking a challenging role where I can leverage my expertise to make a significant impact.";
+const deskripsi = `I am a seasoned Technical Consultant with a strong track record in leveraging Microsoft products to deliver robust solutions. My expertise spans across Active Directory, Microsoft 365 Entra, Microsoft 365 Security, and Microsoft Intune. With a proven ability to understand and solve complex technical challenges, I have helped organizations streamline their IT infrastructure and enhance their security posture.
+<br><br>
+In addition to my consulting experience, I have hands-on experience as a back-end .NET Developer. For a year, I contributed to enterprise projects, where I developed scalable and efficient backend solutions. My dual expertise in consulting and development allows me to bridge the gap between technical implementation and strategic planning, ensuring that projects are not only well-executed but also aligned with business goals.
+<br><br>
+I am passionate about staying current with technological advancements and continuously seek opportunities to expand my knowledge and skills. I thrive in collaborative environments and am committed to driving innovation and excellence in every project I undertake.`;
+
 const deskripsi2 = "C# / .NET back-end developer with expertise in building scalable and reliable applications. Proficient in designing and implementing microservices architectures, RESTful APIs, and cloud-native solutions. Experienced in building serverless applications on Azure";
 
 function updateElementById(id, value) {
@@ -17,5 +22,8 @@ updateElementById("footer", footerText + '&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http
 updateElementById("deskripsi", deskripsi);
 updateElementById("deskripsi2", deskripsi2);
 updateElementById("namalengkap", namaLengkap);
+
+
+
 
 
